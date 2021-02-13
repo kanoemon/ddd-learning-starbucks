8dd9af4cdc906211fa0a612c0895d191843ccc9a
kanoemon <kanoemon.cho@gmail.com>
2021-02-13 19:43:56 +0900

    Add DrinkPrice Domain Model

--- src/productmanagement/domain/model/DrinkPrice.ts
+++ src/productmanagement/domain/model/DrinkPrice.ts
@@ -1,4 +1,25 @@
+import { MenuPrice } from "./MenuPrice";
+import { Size } from "./Size";
+
 export class DrinkPrice {
-  constructor() {
+  private _size: Size;
+  private _menuPrice: MenuPrice;
+
+  constructor(aSize: Size, aMenuPrice: MenuPrice) {
+    this._size = aSize;
+    this._menuPrice = aMenuPrice;
+  }
+
+  get size(): Size {
+    return this._size;
+  }
+
+  get menuPrice(): MenuPrice {
+    return this._menuPrice;
+  }
+
+  equals(aDrinkPrice: DrinkPrice): boolean {
+    return this._size === aDrinkPrice.size
+            && this._menuPrice.equals(aDrinkPrice.menuPrice);
   }
 }
