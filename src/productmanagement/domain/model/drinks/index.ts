06abe9a86c52d431a583fe0947cfb184816d28a5
kanoemon <kanoemon.cho@gmail.com>
2021-02-23 13:24:26 +0900

    Delete files

--- src/productmanagement/domain/model/drinks/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './Drink';
-export * from './DrinkId';
-export * from './DrinkSize';
-export * from './DrinkPrice';
-export * from './DrinkRepository';
-export * from './DrinkService';
