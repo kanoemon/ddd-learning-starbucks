51f6cdc33f0df7088e1cd16ed2c7000430972821
kanoemon <kanoemon.cho@gmail.com>
2021-02-28 21:25:37 +0900

    repositoryをDIする

--- api/src/__tests__/integration/controllers/products/beverage.controller.test.ts
+++ api/src/__tests__/integration/controllers/products/beverage.controller.test.ts
@@ -1,9 +1,12 @@
 import {BeveragesController} from '../../../../controllers/products';
+import { Sqlite3BeverageRepository } from '../../../../repositories';
 
 describe('BeveragesController(integration)', () => {
   describe('getDetails()', () => {
     it ('ok', async () => {
-      const controller = new BeveragesController();
+      const controller = new BeveragesController(
+        new Sqlite3BeverageRepository()
+      );
       const result = await controller.getDetails(1);
       console.log(result);
     });
--- api/src/controllers/products/beverages.controller.ts
+++ api/src/controllers/products/beverages.controller.ts
@@ -1,10 +1,14 @@
+import { repository } from '@loopback/repository';
 import {get, param, response, getModelSchemaRef} from '@loopback/rest';
 import {Sqlite3BeverageRepository} from '../../repositories';
 import {GetBeverageUseCase} from '../../usecases/products/beverages/get-beverage';
 import {GetBeverageModel} from './';
 
 export class BeveragesController {
-  constructor() {}
+  constructor(
+    @repository(Sqlite3BeverageRepository)
+    private beverageRepository: Sqlite3BeverageRepository
+  ) {}
 
   @get('/products/beverages/{id}')
   @response(200, {
@@ -18,7 +22,7 @@ export class BeveragesController {
     @param.path.number('id') id: number,
   ): Promise<GetBeverageModel> {
     const usecase: GetBeverageUseCase = new GetBeverageUseCase(
-      new Sqlite3BeverageRepository(),
+      this.beverageRepository
     );
     await usecase.handle({
       id: id,
