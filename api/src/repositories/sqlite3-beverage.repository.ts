ab7743b15244e3f3ce64d1954aa4e11b5f682999
kanoemon <kanoemon.cho@gmail.com>
2021-03-01 22:46:53 +0900

    Add datasouce

--- api/src/repositories/sqlite3-beverage.repository.ts
+++ api/src/repositories/sqlite3-beverage.repository.ts
@@ -1,8 +1,13 @@
+import { inject } from '@loopback/core';
+import { DbDataSource, StarbucksDataSource } from '../datasources';
 import {Beverage, BeverageId} from '../domain/models/products/beverages';
 import {BeverageRepository} from '../domain/models/products/beverages/beverage-repository';
 
 export class Sqlite3BeverageRepository implements BeverageRepository {
   private _beverages: Beverage[] = [];
+
+  //constructor(@inject('datasources.starbucks') private dataSource: StarbucksDataSource) {}
+  constructor(@inject('datasources.db') private dataSource: DbDataSource) {}
 
   async findById(aBeverageId: BeverageId): Promise<Beverage | null> {
     const beverages: Beverage[] = this._beverages.filter(beverage =>
