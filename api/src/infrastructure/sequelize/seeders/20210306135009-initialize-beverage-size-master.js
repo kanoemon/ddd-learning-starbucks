432189cd758bd8b8548a898ecdcdef53961171cf
kanoemon <kanoemon.cho@gmail.com>
2021-03-06 22:54:23 +0900

    Run migration

---
+++ api/src/infrastructure/sequelize/seeders/20210306135009-initialize-beverage-size-master.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('BeverageSizeMaster', [
+      {sizeId: 1, name: 'short'},
+      {sizeId: 2, name: 'tall'},
+      {sizeId: 3, name: 'grande'},
+      {sizeId: 4, name: 'venti'}
+    ], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
